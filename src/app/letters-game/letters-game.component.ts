6c57c3b95351e3ff03077376a50588ffded6f529
Shaun H <shaunhegarty+git@gmail.com>
2018-03-03 01:33:34 +0000

    Added method for checking if entered word is contained within the word mix

--- src/app/letters-game/letters-game.component.ts
+++ src/app/letters-game/letters-game.component.ts
@@ -9,9 +9,58 @@ export class LettersGameComponent implements OnInit {
 
   lettersMix = 'SERRATION';
 
+  enteredWord = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  checkWords(innerWord: String): void {
+    const contains = this.isWordContainingWord(innerWord, this.lettersMix);
+    if (contains) {
+      console.log(innerWord + ' is in ' + this.lettersMix);
+    } else {
+      console.log(innerWord + ' is NOT in ' + this.lettersMix);
+    }
+  }
+
+  isWordContainingWord(innerWord: String, outerWord: String): boolean {
+    if (!innerWord || !outerWord || !innerWord.length || !outerWord.length) {
+      throw new Error('isWordContainingWord requires two strings to be passed.');
+    }
+
+    const counts: Map<String, number> = new Map;
+    const trimmedInner = innerWord.replace(/\s+/g, '');
+    const trimmedOuter = outerWord.replace(/\s+/g, '');
+
+    console.log('checking: ' + trimmedInner + ' and ' + trimmedOuter);
+
+    if (trimmedOuter.length < trimmedInner.length) {
+      return false;
+    }
+
+    for (const letter of trimmedOuter.toLowerCase().split('')) {
+      if (!counts[letter]) {
+        counts[letter]  = 0;
+      }
+      counts[letter]++;
+    }
+
+    console.log(counts);
+
+    for (const letter of trimmedInner.toLowerCase().split('')) {
+      if (!counts[letter]) {
+        counts[letter]  = 0;
+      }
+      counts[letter]--;
+    }
+
+    return Object.keys(counts).reduce(function(isWordContainingWord, key) {
+      console.log(key + ': ' + counts[key]);
+      return isWordContainingWord && !(counts[key] < 0);
+    }, true);
+
   }
 
 }
